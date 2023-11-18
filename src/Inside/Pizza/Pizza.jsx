bbfd23d74c44c4643385da493e82d5f92ba3ae49
gokul <golubhatt2255@gmail.com>
2023-11-18 22:15:04 +0530

    .

--- src/Inside/Pizza/Pizza.jsx
+++ src/Inside/Pizza/Pizza.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Pizza.css'
-
+import img_pizza1 from '../FoodAssets/img_pizza1.avif'
+import img_pizza2 from '../FoodAssets/img_pizza2.avif'
+import img_pizza3 from '../FoodAssets/img_pizza3.avif'
 const Pizza = () => {
   return (
     <div className='pizza'>
-        <h1>hello This is pizza</h1>
-      
+      <div className="imge">
+        <img src={img_pizza1} alt="" />
+        <img src={img_pizza2} alt="" />
+        <img src={img_pizza3} alt="" />
+        </div>
+     
     </div>
   )
 }
